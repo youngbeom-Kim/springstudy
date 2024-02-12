1be08053adb747d9f448109e3a4fb92b27f65eae
youngbeom-Kim <abcd450196@gmail.com>
2024-02-12 18:55:01 +0900

    feat: 싱글톤 컨테이너에 대한 개념 정리 및 테스트 @Configuration에 대한 이해

---
+++ core/src/test/java/hello/core/singletone/StatefulService.java
@@ -0,0 +1,21 @@
+package hello.core.singletone;
+
+public class StatefulService {
+
+    private int price; //상태를 유지하는 필드 10000 -> 20000
+
+//    public void order(String name, int price) {
+//        System.out.println("name = " + name + " price = " + price);
+//        this.price = price; //여기가 문제!
+//    }
+
+    //위 stateful 문제 해결 방법
+    public int order(String name, int price) {
+        System.out.println("name + = " + name + "price = " + price);
+        return price;
+    }
+
+//    public int getPrice() {
+//        return price;
+//    }
+}
